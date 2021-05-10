e453bbd0f9a09e85819b2ca599e255aef087ae45
Ben <benehamel@gmail.com>
2021-05-10 14:36:26 -0230

    fixed up layout, added grid layout

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -1,39 +1,42 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-                <nav class="navbar">
-                    <a href="./index.html" class="nav-logo">Ben Hamel Portfolio</a>               
-                    <!-- ul.nav-menu>.nav-item*3>a.nav-link shorcut for html below-->
-                    <ul class="nav-menu">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">About</a>   
-                        </li>
-                        <li class="nav-item">
-                            <a href="./project-one.html" class="nav-link">Project 1</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">Project 2</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">Project 3</a>
-                        </li>
-                    </ul>
-                    
-                    <div class="hamburger">
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                    </div>   
-                </nav>
+        <header class="header">
+            <nav class="navbar">
+                <a href="./index.html" class="nav-logo">Ben Hamel Portfolio</a>               
+                <!-- ul.nav-menu>.nav-item*3>a.nav-link shorcut for html below-->
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">About</a>   
+                    </li>
+                    <li class="nav-item">
+                        <a href="./project-one.html" class="nav-link">Project 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Project 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Project 3</a>
+                    </li>
+                </ul>
+                
+                <div class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>   
+            </nav>
+        </header>
         `
     }
 }
 
-customElements.define('page-header', MyHeader)
+customElements.define('page-header', MyHeader) 
 
 class MyFooter extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `Ben Hamel 2021`
+        this.innerHTML = `
+        <footer> Ben Hamel 2021 </footer>`
     }
 }
 
