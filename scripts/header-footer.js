f30374063f3f0646a6af15f2b935093f02289eba
Ben <benehamel@gmail.com>
2021-05-10 15:03:56 -0230

    added about page

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -7,7 +7,7 @@ class MyHeader extends HTMLElement {
                 <!-- ul.nav-menu>.nav-item*3>a.nav-link shorcut for html below-->
                 <ul class="nav-menu">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">About</a>   
+                        <a href="./about.html" class="nav-link">About</a>   
                     </li>
                     <li class="nav-item">
                         <a href="./project-one.html" class="nav-link">Project 1</a>
